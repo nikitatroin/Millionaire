c81bc8cf12ae4c67e073bd8373102fd7f7113885
[nikitatroin] <[nikitatroin@yandex.ru]>
2021-12-30 15:16:05 +0800

    done

---
+++ Millionaire/Models/Answers.swift
@@ -0,0 +1,13 @@
+//
+//  Answers.swift
+//  Millionaire
+//
+//  Created by Никита Троян on 29.12.2021.
+//
+
+import Foundation
+
+enum AllAnswers: String {
+    case first = "I"
+    case second = "Russia"
+}
